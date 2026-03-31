e15db21bb4e9048f68276f78631d34424b957b2c
Arokh Slade <arokhslade@gmail.com>
2026-03-31 17:58:11 +0200

    renamed import plugin file

--- source/addons/armachinko_plugin/armachinko_plugin.gd
+++ source/addons/armachinko_plugin/armachinko_plugin.gd
@@ -1,7 +1,7 @@
 @tool
 extends EditorPlugin
 
-var post_import_plugin = preload("res://addons/armachinko_plugin/asset_import_plugin.gd")
+var post_import_plugin = preload("res://addons/armachinko_plugin/import_plugin.gd")
 var post_import_plugin_instance
 
 func _enter_tree():
